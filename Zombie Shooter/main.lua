1f6da9dcff94e0b324a9f3211fee419568f3c8ce
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2024-01-03 14:21:01 +0200

    :sparkles: Add player movement and orientation

--- Zombie Shooter/main.lua
+++ Zombie Shooter/main.lua
@@ -8,13 +8,43 @@ function love.load()
     player = {}
     player.x = love.graphics.getWidth() / 2
     player.y = love.graphics.getHeight() / 2
-
+    player.speed = 180
+    player.orientation = 0
+    
+    offsets = {}
+    offsets.playerXOffset = sprites.player:getWidth() / 2
+    player.playerYOffset = sprites.player:getHeight() / 2
 end
 
 function love.update(dt)
+    handlePlayerMovement(dt)
 end
 
 function love.draw()
     love.graphics.draw(sprites.background, 0, 0)
-    love.graphics.draw(sprites.player, player.x, player.y)
+    love.graphics.draw(sprites.player, player.x, player.y, player.orientation, nil, nil, offsets.playerXOffset, player.playerYOffset)
+end
+
+function handlePlayerMovement(dt)
+    if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
+        player.x = player.x + player.speed * dt
+    end
+
+    if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
+        player.x = player.x - player.speed * dt
+    end
+
+    if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
+        player.y = player.y - player.speed * dt
+    end
+
+    if love.keyboard.isDown("s") or love.keyboard.isDown("down") then
+        player.y = player.y + player.speed * dt
+    end
+
+    player.orientation = playerMouseAngle()
+end
+
+function playerMouseAngle()
+    return math.atan2(love.mouse.getY() - player.y, love.mouse.getX() - player.x)
 end
