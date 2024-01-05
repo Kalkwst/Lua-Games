eb078cc89702f35c1811b64a8822f26f21d98189
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2024-01-05 11:21:28 +0200

    :sparkles: Move player code to player.lua file

--- Platformer/main.lua
+++ Platformer/main.lua
@@ -19,15 +19,7 @@ function love.load()
     world:addCollisionClass('Platform')
     world:addCollisionClass('Danger')
 
-    player = world:newRectangleCollider(360, 100, 40, 100, {
-        collision_class = "Player"
-    })
-    player:setFixedRotation(true)
-    player.speed = 240
-    player.animation = animations.idle
-    player.isMoving = false
-    player.isGrounded = true
-    player.direction = 1
+    require('player')
 
     platform = world:newRectangleCollider(250, 400, 300, 100, {
         collision_class = "Platform"
@@ -64,44 +56,7 @@ end
 
 function love.draw()
     world:draw()
-
-    if player.body then
-        local px, py = player:getPosition()
-        player.animation:draw(sprites.playerSheet, px, py, nil, 0.25 * player.direction, 0.25, 130, 300)
-    end
-
-end
-
-function handlePlayerMovement(dt)
-    local px, py = player:getPosition()
-
-    if (love.keyboard.isDown("d") or love.keyboard.isDown("right")) then
-        player:setX(px + player.speed * dt)
-        player.isMoving = true
-        player.direction = 1
-    end
-
-    if (love.keyboard.isDown("a") or love.keyboard.isDown("left")) then
-        player:setX(px - player.speed * dt)
-        player.isMoving = true
-        player.direction = -1
-    end
-end
-
-function handleDangerCollisions(dt)
-    if player:enter('Danger') then
-        player:destroy()
-    end
-end
-
-function handlePlayerAnimations()
-    if player.isMoving and player.isGrounded then
-        player.animation = animations.run
-    elseif player.isGrounded and player.isMoving == false then
-        player.animation = animations.idle
-    else
-        player.animation = animations.jump
-    end
+    drawPlayer()
 end
 
 function love.keypressed(key)
